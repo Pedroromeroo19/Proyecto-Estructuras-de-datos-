8c17a550d06527f7592d4daa1e0274192d44cb88
jhossbe <jmbello.24@est.ucab.edu.ve>
2025-01-10 07:05:53 -0400

    Update Equipos.h

--- Equipos.h
+++ Equipos.h
@@ -6,7 +6,7 @@ using namespace std;
 struct Equipos{
     string nombre; 
     int numero_de_soldados; 
-    soldado *listaJugadores; 
+    Soldados *listaJugadores; 
     Equipos *prox; 
 }; 
 
@@ -46,7 +46,7 @@ Equipos *Buscar_Equipo(Equipos *lista, string nombre){
 void agregarUltimoEquipo(Equipos **equipo_listas, string nombre_equipo){
     int cant = 2; 
     int cont = 0; 
-    Equipos *NuevoEquipo = nullptr; 
+    Equipos *NuevoEquipo = CrearEquipo(nombre_equipo); 
     while (cont < cant){
         if(listaVaciaEquipos(*equipo_listas)){
             (*equipo_listas) = NuevoEquipo; 
@@ -64,7 +64,7 @@ void agregarUltimoEquipo(Equipos **equipo_listas, string nombre_equipo){
 void EliminarEquipo(Equipos **lista, string nombre){
     Equipos *actual = (*lista); 
     Equipos *anterior = nullptr; 
-    soldado *eliminado = nullptr;
+    Soldados::soldado* eliminado = nullptr;
 
     if(listaVaciaEquipos(*lista)){
         cout << "No hay equipos formados! Crea un equipo para poder eliminarlo. " << endl; 
@@ -81,7 +81,7 @@ void EliminarEquipo(Equipos **lista, string nombre){
                 (*lista) = (*lista)->prox;
             }else{
                 anterior->prox = actual->prox; 
-                soldado *aux = eliminado;
+                Soldados::soldado *aux = eliminado;
                 if(eliminado != nullptr){
                     while(aux != nullptr){
                         eliminado = eliminado->next; 
@@ -90,26 +90,47 @@ void EliminarEquipo(Equipos **lista, string nombre){
                     }
                 }
             }
-            delete actual; 
         }
     }
+    delete actual; 
 }
 
-void AgregarSoldadoEquipoPorNombre(Equipos **lista, string nombre, soldado *lista_jugadores){
-    Equipos *actual; 
+void AgregarSoldadoEquipoPorNombre(Equipos **lista, string nombre, Soldados lista_jugadores){
+    Equipos *actual = *lista; 
     actual = Buscar_Equipo(*lista, nombre);
+    
     
     if(listaVaciaEquipos(actual)){
         cout << "Equipo no encontrado. Intenta con otro nombre!" << endl; 
     }else{
         int cant;
+        cout << "Indique la cantidad de soldados que desea agregar: " << endl;
         cin >> cant; 
         for(int i = 0; i<cant; i++){
             string nombre_soldado; 
 
-            leerlistsoldado(lista_jugadores);
-            Agregar_Ultimo_especifico_Soldado(nombreSoldado(), vida(), &actual->listaJugadores);
+            lista_jugadores.agregar_soldados_por_zmb();
+            lista_jugadores.agregar_espec_soldados(-1,lista_jugadores.nombreSoldado(), lista_jugadores.vida());
         }
         actual->numero_de_soldados = cant; 
+    }
+}
+
+void asignarEstacionLlegadaAleatoria(vector<string>& estaciones, Equipos* listaEquipos) {
+    if (listaVaciaEquipos(listaEquipos)) {
+        cout << "No hay equipos disponibles para asignar estación de llegada." << endl;
+        return;
+    }
+
+    srand(static_cast<unsigned>(time(nullptr))); // Inicializar generador aleatorio
+    int indiceLlegada = rand() % estaciones.size();
+    string estacionLlegada = estaciones[indiceLlegada];
+
+    cout << "Estación de llegada asignada aleatoriamente: " << estacionLlegada << endl;
+
+    Equipos* actual = listaEquipos;
+    while (actual != nullptr) {
+        cout << "Equipo " << actual->nombre << " tiene como estación de llegada: " << estacionLlegada << endl;
+        actual = actual->prox;
     }
 }
